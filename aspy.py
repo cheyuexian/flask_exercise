d6188786a44bab2f3d2c134bed2afc25c185391b
cheyuexian <694547990@qq.com>
2017-08-19 01:25:59 +0800

    上下文管理器demo

--- aspy.py
+++ aspy.py
@@ -1,5 +1,6 @@
 
-
+'''上下文管理器的原理
+'''
 class PyContextManager:
 	def __enter__(self):
 		self.openFile = open(self.filename,self.mode)
@@ -12,7 +13,7 @@ class PyContextManager:
 	def __init__(self,filename,mode):
 		self.filename = filename
 		self.mode = mode
-		
+
 
 with PyContextManager("1.txt",'w') as f:
 	f.write("1")
