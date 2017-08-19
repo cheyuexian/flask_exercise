b10101572fe44954bf269289962fe3fcb00285d9
cheyuexian <694547990@qq.com>
2017-08-20 00:33:40 +0800

    重定向之前

--- hello1.py
+++ hello1.py
@@ -1,38 +1,35 @@
 
-# from flask import Flask,render_template
-# from flask_script import Manager
 
-
-# app = Flask(__name__)
-# manager = Manager(app)
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# # @app.route('/user/<name>')
-# # def index(name):
-# #     return render_template('user.html',name = name)
-# @app.route('/user/<name>')
-# def user(name):
-#     return render_template('user.html', name=name)
-# if __name__ == '__main__':
-#   	manager.run()
 from datetime import datetime
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from  wtforms import StringField,SubmitField
+from wtforms.validators import Required
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+class NameForm(FlaskForm):
+	name = StringField('what is your nam?',validators=[Required()])
+	submit = SubmitField('Submit')
 
-@app.route('/')
+
+
+@app.route('/',methods = ['GET','POST'])
 def index():
-    return render_template('index.html',current_time=datetime.utcnow())
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html',form=form,name=name,current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
